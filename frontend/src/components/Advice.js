906c69d9cad60028d69f15e5b986f6573f5f6b5b
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-17 15:22:32 +0000

    updt.

--- frontend/src/components/Advice.js
+++ frontend/src/components/Advice.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { checkPropTypes } from 'prop-types';
+import axios from 'axios'
 
 class Advice extends React.Component {
     state = {
         advice: 'nothing'
     }
+
+    componentDidMount(){
+        axios.get('/advice')
+            .then(req => {
+                console.log(req.data);
+            }).catch(err => {
+                console.log(err);
+            })
+    }
+
     render(){
     return(
         <div> Advice: {this.state.advice} </div>
